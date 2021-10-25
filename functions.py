089131e033b95fd99a795e7d8b6721ce69d75ace
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-25 15:54:30 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,7 +13,8 @@ def cost(theta, X, y, learningRate):  # learninRate正则化参数（lambda）
     cross_cost = np.multiply(-y, np.log(sigmoid(X * theta.T))) - np.multiply((1 - y), np.log(1 - sigmoid(X * theta.T)))
     # 正则化部分
     reg = (learningRate / (2 * len(X))) * np.sum(np.power(theta[1:], 2))
-    return np.sum(cross_cost) / len(X) + reg
+    whole_cost = np.sum(cross_cost) / len(X) + reg
+    return whole_cost
 
 
 def gradientDescent(theta, X, y, learningRate):
@@ -22,9 +23,9 @@ def gradientDescent(theta, X, y, learningRate):
     y = np.matrix(y)
 
     parameters = int(theta.ravel().shape[1])
-    #计算预测误差
+    # 计算预测误差
     error = sigmoid(X * theta.T) - y
-    #计算梯度
+    # 计算梯度
     grad = ((X.T * error) / len(X)).T + ((learningRate / len(X)) * theta)
     grad[0, 0] = np.sum(np.multiply(error, X[:, 0])) / len(X)
     return np.array(grad).ravel()
