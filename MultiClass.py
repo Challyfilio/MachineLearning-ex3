b64920e6353bbd8bb0d59cf568aa323997acbc85
Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-25 11:55:13 +0800

    Create vcs.xml

--- MultiClass.py
+++ MultiClass.py
@@ -30,3 +30,15 @@ if __name__ == '__main__':
     print(data)
 
     print(data['X'].shape, data['y'].shape)
+
+    rows = data['X'].shape[0]
+    params = data['X'].shape[1]
+    all_theta = np.zeros((10,params+1))
+    X=np.insert(data['X'],0,values=np.ones(rows),axis=1)
+
+    theta = np.zeros(params+1)
+
+    y_0= np.array([1 if label==0 else 0 for label in data['y']])
+    y_0= np.reshape(y_0,(rows ,1))
+
+    print(X.shape,y_0.shape,theta.shape,all_theta.shape)
