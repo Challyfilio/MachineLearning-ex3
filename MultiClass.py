1ec8881b5c66543c18dfbd79ae40254135129442
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-26 09:25:11 +0800

    none

--- MultiClass.py
+++ MultiClass.py
@@ -3,7 +3,36 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 from scipy.optimize import minimize
-from functions import *
+
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+def cost(theta, X, y, learningRate):  # learninRate正则化参数（lambda）
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+
+    cross_cost = np.multiply(-y, np.log(sigmoid(X * theta.T))) - np.multiply((1 - y), np.log(1 - sigmoid(X * theta.T)))
+    # 正则化部分
+    reg = (learningRate / (2 * len(X))) * np.sum(np.power(theta[1:], 2))
+    whole_cost = np.sum(cross_cost) / len(X) + reg
+    return whole_cost
+
+
+def gradientDescent(theta, X, y, learningRate):
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+
+    parameters = int(theta.ravel().shape[1])
+    # 计算预测误差
+    error = sigmoid(X * theta.T) - y
+    # 计算梯度
+    grad = ((X.T * error) / len(X)).T + ((learningRate / len(X)) * theta)
+    grad[0, 0] = np.sum(np.multiply(error, X[:, 0])) / len(X)
+    return np.array(grad).ravel()
 
 
 def one_vs_all(X, y, num_labels, learning_rate):
@@ -66,7 +95,7 @@ if __name__ == '__main__':
     all_theta = one_vs_all(data['X'], data['y'], 10, 1)
     print(all_theta)
 
-    y_pred = predict_all(data['X'],all_theta)
-    correct = [1 if a == b else 0 for (a,b) in zip(y_pred,data['y'])]
-    accuracy = (sum(map(int,correct)))/float(len(correct))
-    print('accuracy='+str(accuracy))
+    y_pred = predict_all(data['X'], all_theta)
+    correct = [1 if a == b else 0 for (a, b) in zip(y_pred, data['y'])]
+    accuracy = (sum(map(int, correct))) / float(len(correct))
+    print('accuracy=' + str(accuracy))
