cf4ca3a742ec110748459f532ce1c9a2ecde6704
✨Challyfilio✨ <challyfilio4368@vip.qq.com>
2021-10-28 11:05:34 +0800

    none

--- MultiClass.py
+++ MultiClass.py
@@ -1,3 +1,4 @@
+import matplotlib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -26,7 +27,7 @@ def gradientDescent(theta, X, y, learningRate):
     X = np.matrix(X)
     y = np.matrix(y)
 
-    parameters = int(theta.ravel().shape[1])
+    parameters = int(theta.ravel().shape[1])  # theta的数量
     # 计算预测误差
     error = sigmoid(X * theta.T) - y
     # 计算梯度
@@ -65,6 +66,7 @@ def predict_all(X, all_theta):
     all_theta = np.matrix(all_theta)
     # 计算样本属于每一类的概率
     h = sigmoid(X * all_theta.T)
+    print(h)
     # 每个样本中预测概率最大值
     h_argmax = np.argmax(h, axis=1)
     # 索引+1
@@ -73,29 +75,44 @@ def predict_all(X, all_theta):
 
 
 if __name__ == '__main__':
+    #数据集
     data = loadmat("ex3data1.mat")
     print(data)
 
     print(data['X'].shape, data['y'].shape)
 
-    rows = data['X'].shape[0]
-    params = data['X'].shape[1]
-    all_theta = np.zeros((10, params + 1))
-    X = np.insert(data['X'], 0, values=np.ones(rows), axis=1)
+    # 数据可视化
+    sample_idx = np.random.choice(np.arange(data['X'].shape[0]), 100)
+    sample_images = data['X'][sample_idx, :]
+    print(sample_images)
 
-    theta = np.zeros(params + 1)
+    fig, ax = plt.subplots(nrows=10, ncols=10, sharex=True, sharey=True, figsize=(12, 12))
+    for r in range(10):
+        for c in range(10):
+            ax[r, c].matshow(np.array(sample_images[10 * r + c].reshape((20, 20))).T, cmap=matplotlib.cm.binary)
+            plt.xticks(np.array([]))
+            plt.yticks(np.array([]))
+    plt.show()
 
-    y_0 = np.array([1 if label == 0 else 0 for label in data['y']])
-    y_0 = np.reshape(y_0, (rows, 1))
-
-    print(X.shape, y_0.shape, theta.shape, all_theta.shape)
-    # 标签
-    print(np.unique(data['y']))
-
-    all_theta = one_vs_all(data['X'], data['y'], 10, 1)
-    print(all_theta)
-
-    y_pred = predict_all(data['X'], all_theta)
-    correct = [1 if a == b else 0 for (a, b) in zip(y_pred, data['y'])]
-    accuracy = (sum(map(int, correct))) / float(len(correct))
-    print('accuracy=' + str(accuracy))
+    # rows = data['X'].shape[0]
+    # params = data['X'].shape[1]
+    # all_theta = np.zeros((10, params + 1))
+    # X = np.insert(data['X'], 0, values=np.ones(rows), axis=1)
+    #
+    # theta = np.zeros(params + 1)
+    #
+    # y_0 = np.array([1 if label == 0 else 0 for label in data['y']])
+    # y_0 = np.reshape(y_0, (rows, 1))
+    #
+    # print(X.shape, y_0.shape, theta.shape, all_theta.shape)
+    # # 标签
+    # print(np.unique(data['y']))
+    #
+    # all_theta = one_vs_all(data['X'], data['y'], 10, 1)
+    # # print(all_theta)
+    #
+    # y_pred = predict_all(data['X'], all_theta)
+    # print(y_pred)
+    # correct = [1 if a == b else 0 for (a, b) in zip(y_pred, data['y'])]
+    # accuracy = (sum(map(int, correct))) / float(len(correct))
+    # print('accuracy=' + str(accuracy))
---
+++ README.md
@@ -0,0 +1,5 @@
+# MachineLearning-ex3
+
+#### MultiClass.py
+- 实现手写数字（0到9）的识别。
+- 扩展之前的逻辑回归，并将其应用于一对多的分类。
